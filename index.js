0c5ee44217b0f901934f9057e2e0c5c80655e80f
Labib <risenotfall123@gmail.com>
2023-03-30 14:47:32 -0400

    added night/day mode and fixed some issues on nav bar

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ function typeText(text, index) {
 window.onload = () => {
   typeText(typedText, 0);
 }
+
+const modeButton = document.getElementById('mode-button');
+const body = document.body;
+const isDark = localStorage.getItem('dark') === 'true';
+const socialIcons = document.querySelector('.social-media');
+
+if (isDark) {
+  body.classList.add('dark-mode');
+}
+
+updateModeButtonText();
+
+modeButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  body.classList.toggle('dark-mode');
+  localStorage.setItem('dark', body.classList.contains('dark-mode'));
+  updateModeButtonText();
+});
+
+function updateModeButtonText() {
+  if (body.classList.contains('dark-mode')) {
+    modeButton.innerText = '☀';
+    socialIcons.classList.add('dark-mode');
+  } else {
+    modeButton.innerText = '🔆';
+    socialIcons.classList.remove('dark-mode');
+  }
+}
