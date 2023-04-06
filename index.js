df1cd1ed092af2826f56530eec0c395a50d14388
Labib <risenotfall123@gmail.com>
2023-04-06 13:43:33 -0400

    added scroll up button

--- index.js
+++ index.js
@@ -39,3 +39,18 @@ function updateModeButtonText() {
     socialIcons.classList.remove('dark-mode');
   }
 }
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#scroll-to-top').fadeIn();
+    } else {
+      $('#scroll-to-top').fadeOut();
+    }
+  });
+
+  $('#scroll-to-top').click(function() {
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+  });
+});
